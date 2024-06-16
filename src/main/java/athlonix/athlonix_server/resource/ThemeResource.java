dea95ddaf8ff9fd0ee38b93b177aa2fdf571a78e
Metololo <93775692+Metololo@users.noreply.github.com>
2024-06-16 20:08:04 +0200

    feat: setup springboot app

---
+++ src/main/java/athlonix/athlonix_server/resource/ThemeResource.java
@@ -0,0 +1,43 @@
+package athlonix.athlonix_server.resource;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.nio.file.Paths.get;
+
+@RestController
+@RequestMapping("/theme")
+public class ThemeResource {
+
+    public static final String THEME_DIRECTORY = "./themes";
+
+    @GetMapping("{theme}")
+    public ResponseEntity<Resource> downloadTheme(@PathVariable("theme") String theme) throws IOException {
+        String themeFile = theme + ".css";
+        Path filePath = get(THEME_DIRECTORY).toAbsolutePath().normalize().resolve(themeFile);
+        System.out.println(filePath);
+        if(!Files.exists(filePath)) {
+            throw new FileNotFoundException(theme + " file not found");
+        }
+        Resource resource = new UrlResource(filePath.toUri());
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("File-Name", themeFile);
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment;File-Name=" + resource.getFilename());
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(Files.probeContentType(filePath)))
+                .headers(headers).body(resource);
+    }
+
+}
---
+++ src/test/java/athlonix/athlonix_server/AthlonixServerApplicationTests.java
@@ -0,0 +1,13 @@
+package athlonix.athlonix_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthlonixServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
