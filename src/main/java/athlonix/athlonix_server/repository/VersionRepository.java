c3a3330e4c1d192768a2d0bf8ca34d3ca50152d2
Metololo <93775692+Metololo@users.noreply.github.com>
2024-06-25 20:14:24 +0200

    feat: add version repository

---
+++ src/main/java/athlonix/athlonix_server/repository/VersionRepository.java
@@ -0,0 +1,17 @@
+package athlonix.athlonix_server.repository;
+
+import athlonix.athlonix_server.model.Theme;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.nio.file.Paths.get;
+
+public class VersionRepository {
+    public static final String VERSION_DIRECTORY = "./versions";
+    public String getMostRecentVersionPath() {
+    }
+}
